ef7bfb25c9af0a9b36bb7ed501815c0e18089aa2
amitznati-work-hp <amitznati@gmail.com>
2019-10-04 00:52:36 +0300

    font selector with menu list

--- src/components/core/CoreFontSelector.js
+++ src/components/core/CoreFontSelector.js
@@ -1,47 +1,118 @@
+
 import React from 'react';
-import FontPicker from 'font-picker-react';
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroller';
+import {Popover, Button, MenuItem, MenuList} from '@material-ui/core';
 import config from '../../config';
 
- class CoreFontSelector extends React.Component {
+class CoreFontPicker extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			activeFonts: [],
+			hasMore: false,
+			anchorEl: null,
+			allFontsNames: []
+		}
+		this.loadFonts();
+	}
 
+	loadFonts = () => {
+		this.xhr1 = new XMLHttpRequest();
+        /*eslint-disable */
+        this.xhr1.open('GET', `https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=${config.googleFontAPIKey}`, true);
+        /*eslint-enable */
+        this.xhr1.send();
+		this.xhr1.addEventListener('readystatechange', this.fontsLoaded, false);
+	};
 
-	fontLoaded = () => {
-		if (this.xhr1.readyState === 4 && this.xhr1.status === 200) {
-            // this.aemmessages = JSON.parse(this.xhr1.responseText);
-			// localStorage.setItem('uiErrormessages', this.xhr1.responseText);
-			if (this.props.onChange) {
-				this.props.onChange(this.nextFontFamily);
+	fontsLoaded = () => {
+		if (this.xhr1.readyState === 4 && this.xhr1.status === 200 && this.xhr1.responseText) {
+			const allFonts = JSON.parse(this.xhr1.responseText);
+			console.log(allFonts);
+			if (allFonts) {
+				const allFontsNames = allFonts.items.map(f => f.family);
+				const activeFonts = allFontsNames.slice(0, 30);
+				const hasMore = !(activeFonts.length === allFontsNames.length);
+				this.setState({allFontsNames, activeFonts, hasMore});
 			}
         }
 	}
+	// const allFontsSlice = allFonts.slice(0, 30);
+	// const [anchorEl, setAnchorEl] = React.useState(null);
+	// const [activeNames, setActiveNames] = React.useState(allFontsSlice);
+	// const [hasMore, setHasMore] = React.useState(!(allFontsSlice.length === allFonts.length));
+	handleChange = (font) => {
+		const {handleChange} = this.props;
+		handleChange && handleChange(font);
+		this.setState({anchorEl: null})
+	};
 
-	loadFont = (nextFont) => {
-		const {fontStyle = 'regular', fontWeight = '100'} = this.props;
-		this.xhr1 = new XMLHttpRequest();
-		this.nextFontFamily = nextFont.family;
-        /*eslint-disable */
-        this.xhr1.open('GET', `https://fonts.googleapis.com/css?family=${nextFont.family}:${fontWeight}${fontStyle}`, true);
-        /*eslint-enable */
-        this.xhr1.send();
-		this.xhr1.addEventListener('readystatechange', this.fontLoaded, false);
-	}
+	loadMore = () => {
+		const {activeFonts, allFontsNames} = this.state;
+		const newActiveFonts = allFontsNames.slice(0, activeFonts.length + 30);
+		this.setState({activeFonts: newActiveFonts, hasMore: !(newActiveFonts.length === allFontsNames.length)});
+	};
 
+	renderItems = () => {
+		const {fontWeight, fontStyle} = this.props;
+		const {activeFonts} = this.state;
+		const items = activeFonts.map(font => {
+			return (
+				<MenuItem onClick={() => this.handleChange(font)} key={font} style={{fontFamily: font, fontWeight, fontStyle}}>{font}</MenuItem>
+			)
+		});
+		return items;
+	};
 
 	render() {
-		const {fontStyle = 'regular', fontWeight = '100', fontFamily = 'Raleway'} = this.props;
+		const {value, fontFamily, fontWeight, fontStyle} = this.props;
+		const {anchorEl, hasMore} = this.state;
+		const open = Boolean(anchorEl);
+		const id = open ? 'stroke-color-popover' : undefined;
+		
 		return (
 			<div>
-				<FontPicker
-					apiKey={config.googleFontAPIKey}
-					activeFontFamily={fontFamily}
-					limit={50}
-					onChange={this.loadFont}
-					variants={[fontStyle, fontWeight]}
-					sort="popularity"
-				/>
+				<Button aria-describedby={id} size="small" onClick={(e) => this.setState({anchorEl: e.currentTarget})} style={{fontFamily, fontWeight, fontStyle}}>
+					{value}
+				</Button>
+				<Popover
+					id={id}
+					open={open}
+					anchorEl={anchorEl}
+					onClose={() => this.setState({anchorEl: null})}
+					anchorOrigin={{
+						vertical: 'bottom',
+						horizontal: 'left',
+					}}
+					transformOrigin={{
+						vertical: 'top',
+						horizontal: 'left',
+					}}
+				>
+					<MenuList style={{height: '500px', overflow: 'auto'}}>
+						<InfiniteScroll
+							pageStart={0}
+							loadMore={this.loadMore}
+							hasMore={hasMore}
+							useWindow={false}
+							loader={<div className="loader" key={0}>Loading ...</div>}
+						>
+							
+							{this.renderItems()}
+						</InfiniteScroll>
+					</MenuList>
+				</Popover>
 			</div>
+			
 		);
-	}
+			}
+	
 }
 
-export default CoreFontSelector
+CoreFontPicker.propTypes = {
+	onChange: PropTypes.func,
+	value: PropTypes.any
+};
+
+export default CoreFontPicker;
--- src/components/editLayoutFields/fontProperties.js
+++ src/components/editLayoutFields/fontProperties.js
@@ -2,28 +2,34 @@ import React from 'react';
 //import { makeStyles } from '@material-ui/core/styles';
 import {Grid} from '@material-ui/core';
 import WebFont from 'webfontloader';
-import { CoreNumber, CoreSelect, CoreFontSelector, CoreText } from '../core';
+
+import { CoreNumber, CoreSelect, CoreText, CoreFontSelector } from '../core';
 
 // const useStyles = makeStyles(theme => ({
 // 	progress: {
 // 	  margin: theme.spacing(2),
 // 	},
 // }));
-
-const FontProperties = (props) => {
-
-	const weightOptions =
+const weightOptions =
 	['normal','100','200','300','400','500','600','700','800','900','bold','bolder','lighter'].map((i) => {
 		return {id: i, name: i};
 	});
 
-	const styleOptions =
+const styleOptions =
 	['italic', 'normal'].map((i) => {
 		return {id: i, name: i};
 	});
 
-	const {fontSize, fontWeight, fontStyle, fontFamily, text} = props;
-	const onPropertyChange = (name, value) => {
+class FontProperties extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			fontsNames: []
+		}
+	}
+
+	onPropertyChange = (name, value) => {
+		const {fontWeight, fontStyle, fontFamily, onPropertyChange} = this.props;
 		const selectedFontFamily = name === 'fontFamily' ? value : fontFamily;
 		const selectedFontWeight = name === 'fontWeight' ? value : fontWeight;
 		const selectedFontStyle = name === 'fontStyle' ? value : fontStyle;
@@ -33,51 +39,55 @@ const FontProperties = (props) => {
 			},
 			fontactive: (familyName, fvd) => {
 				console.info(familyName + ' ' + fvd + ' is Active!');
-				props.onPropertyChange && props.onPropertyChange(name, value);
+				onPropertyChange && onPropertyChange(name, value);
 			},
 		});
-	}
+	};
 
-	return (
-		<Grid container>
-			<Grid item xs={12}>
-				<CoreText
-					label="Text"
-					value={text}
-					handleChange={v => props.onPropertyChange && props.onPropertyChange('text', v)}
-				/>
+	render() {
+		const props = this.props;
+		const {fontSize, fontWeight, fontStyle, fontFamily, text} = props;
+		return (
+			<Grid container>
+				<Grid item xs={12}>
+					<CoreText
+						label="Text"
+						value={text}
+						handleChange={v => props.onPropertyChange && props.onPropertyChange('text', v)}
+					/>
+				</Grid>
+				<Grid item xs={12} >
+					<CoreFontSelector
+						value={fontFamily}
+						handleChange={v => this.onPropertyChange('fontFamily', v)}
+					/>
+				</Grid>
+				<Grid item xs={3}>
+					<CoreNumber
+						label="Size"
+						value={fontSize}
+						onChange={v => props.onPropertyChange && props.onPropertyChange('fontSize', v)}
+					/>
+				</Grid>
+				<Grid item xs={3}>
+					<CoreSelect
+						label="Weight"
+						value={fontWeight}
+						options={weightOptions}
+						onChange={v => this.onPropertyChange('fontWeight', v)}
+					/>
+				</Grid>
+				<Grid item xs={4}>
+					<CoreSelect
+						label="Style"
+						value={fontStyle}
+						options={styleOptions}
+						onChange={v => this.onPropertyChange('fontStyle', v)}
+					/>
+				</Grid>
 			</Grid>
-			<Grid item xs={12} >
-				<CoreFontSelector
-					{...{fontFamily, fontStyle, fontWeight}}
-					onChange={v => onPropertyChange('fontFamily', v)}
-				/>
-			</Grid>
-			<Grid item xs={3}>
-				<CoreNumber
-					label="Size"
-					value={fontSize}
-					onChange={v => props.onPropertyChange && props.onPropertyChange('fontSize', v)}
-				/>
-			</Grid>
-			<Grid item xs={3}>
-				<CoreSelect
-					label="Weight"
-					value={fontWeight}
-					options={weightOptions}
-					onChange={v => onPropertyChange('fontWeight', v)}
-				/>
-			</Grid>
-			<Grid item xs={4}>
-				<CoreSelect
-					label="Style"
-					value={fontStyle}
-					options={styleOptions}
-					onChange={v => onPropertyChange('fontStyle', v)}
-				/>
-			</Grid>
-		</Grid>
-	);
+		);
+	}
 };
 
 export default FontProperties;
